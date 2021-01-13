425803c4472e164e8600f6c03d999e8e7e28b001
zhouyaxue <16222@qq.com>
2021-01-13 18:31:57 +0800

    '动态组件和异步组件'

--- src/components/AdvancedUse/index.vue
+++ src/components/AdvancedUse/index.vue
@@ -10,33 +10,40 @@
             {{website.title}}
         </SlotDemo> -->
         <!-- 作用域插槽: 父组件如何获取子组件里的数据 -->
-        <ScopedSlotDemo :url="website.url">
-            <!-- {{website.title}} -->
+        <!-- <ScopedSlotDemo :url="website.url">
             <template v-slot="slotProps">
                 {{slotProps.slotData.title}}
             </template>
-        </ScopedSlotDemo>
+        </ScopedSlotDemo> -->
+        <!-- 动态组件 -->
+        <!-- <component :is="NextTickName"></component> -->
+        <!-- 按需加载组件 -->
+        <FormDemo v-if="showFormDemo"/>
+        <button @click="showFormDemo=true">按钮</button>
     </div>
 </template>
 <script>
 // import CustomVModel from './CustomVModel'
 // import NextTick from './NextTick'
 // import SlotDemo from './SlotDemo'
-import ScopedSlotDemo from './ScopedSlotDemo'
+// import ScopedSlotDemo from './ScopedSlotDemo'
 export default {
     components:{
         // CustomVModel,
         // NextTick,
         // SlotDemo,
-        ScopedSlotDemo
+        // ScopedSlotDemo,
+        FormDemo:()=>import('../BaseUse/FormDemo')
     },
     data(){
         return {
+            showFormDemo:false,
             website:{
                 url:'https://qq.weatherol.com.cn',
                 title:'yaxue',
                 subTitle:'气象在线科技有限公司'
-            }
+            },
+            // NextTickName:'FormDemo',
         }
     }
 }
